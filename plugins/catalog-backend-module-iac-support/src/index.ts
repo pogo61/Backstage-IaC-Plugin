c5149f904c3337c98ddc5ca793ead07e74e6c694
paulpog <paulpog@japarasolutions.com>
2024-04-10 14:54:50 +1000

    Re-defined the README.md to reflect on the changes for Release 2

---
+++ plugins/catalog-backend-module-iac-support/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Adds support for the IaC Support specific entity model (e.g. ResourceComponent or Environment) to the catalog backend plugin.
+ *
+ * @packageDocumentation
+ */
+
+export * from './processor';
+export { catalogModuleIacSupportEntityModel as default } from "./modules";
+
