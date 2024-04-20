65ce065fccf1a8fa91f5361a408e76345eb6c2d7
Paul Pogonoski <paulpog@japarasolutions.com>
2024-04-20 17:39:39 +1000

    Add ia c support to display has subcomponents in frontend (#5)
    * Creating a replacement for HasSubcomponentsCard react component to display IaC entities in the UI
    
    * fixing definition of owning ResourceComponent in examples and fixing the plugin definition

---
+++ plugins/iac-support-frontend/src/components/HasSubcomponentsCard/HasSubcomponentsCard.tsx
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// import {RELATION_HAS_PART,} from '@backstage/catalog-model';
+import {InfoCardVariants, TableOptions} from '@backstage/core-components';
+import React from 'react';
+import {RelatedEntitiesCard} from '@backstage/plugin-catalog';
+import {asResComponentEntities, resComponentEntityColumns, resComponentEntityHelpLink} from './presets';
+
+/** @public */
+export interface HasSubcomponentsCardProps {
+  variant?: InfoCardVariants;
+  tableOptions?: TableOptions;
+  title?: string;
+  relationType?: string;
+}
+
+export function HasSubcomponentsCard(props: HasSubcomponentsCardProps) {
+  const {
+    variant = 'gridItem',
+    tableOptions = {},
+    title = 'Has subcomponents',
+    relationType = 'hasPart'
+  } = props;
+  return (
+      <RelatedEntitiesCard
+          variant={variant}
+          title={title}
+          entityKind="ResourceComponent"
+          relationType={relationType}
+          columns={resComponentEntityColumns}
+          asRenderableEntities={asResComponentEntities}
+          emptyMessage=""
+          emptyHelpLink={resComponentEntityHelpLink}
+          tableOptions={tableOptions}
+      />
+  );
+}
---
+++ plugins/iac-support-frontend/src/components/HasSubcomponentsCard/presets.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  ComponentEntity,
+  Entity
+} from '@backstage/catalog-model';
+import { EntityTable } from '@backstage/plugin-catalog-react';
+import { TableColumn } from '@backstage/core-components';
+
+export const resComponentEntityColumns: TableColumn<ComponentEntity>[] = [
+  EntityTable.columns.createEntityRefColumn({ defaultKind: 'ResourceComponent' }),
+  EntityTable.columns.createOwnerColumn(),
+  EntityTable.columns.createSpecTypeColumn(),
+  EntityTable.columns.createSpecLifecycleColumn(),
+  EntityTable.columns.createMetadataDescriptionColumn(),
+];
+export const resComponentEntityHelpLink: string =
+  'https://backstage.io/docs/features/software-catalog/descriptor-format#kind-component';
+export const asResComponentEntities = (entities: Entity[]): ComponentEntity[] =>
+  entities as ComponentEntity[];
+
+
+
